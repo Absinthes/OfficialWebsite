a802400d03cc948290d00215898bac2e6d1bfd10
杨程超 <1070129872@qq.com>
2024-02-14 13:48:41 +0800

    docs: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,35 @@
-import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import Unocss from "unocss/vite";
 import { join, resolve } from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    Unocss({
-      configFile: resolve(__dirname, "uno.config.ts"),
-    }),
-    createSvgIconsPlugin({
-      iconDirs: [join(__dirname, "./src/assets/svg")],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-      "~/": resolve(__dirname, "src"),
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log("mode: ", mode);
+  console.log("env", env);
+
+  return {
+    base: mode == "development" ? "/" : "/OfficialWebsite/",
+    plugins: [
+      react(),
+      Unocss({
+        configFile: resolve(__dirname, "uno.config.ts"),
+      }),
+      createSvgIconsPlugin({
+        iconDirs: [join(__dirname, "./src/assets/svg")],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "~/": resolve(__dirname, "src"),
+      },
     },
-  },
-  server: {
-    open: true,
-    host: "0.0.0.0",
-  },
+    server: {
+      open: true,
+      host: "0.0.0.0",
+    },
+  };
 });
